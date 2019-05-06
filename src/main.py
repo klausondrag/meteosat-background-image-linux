9cb80311b79881aa7df53f648f5e3c86b38d683d
klausondrag <18405064+klausondrag@users.noreply.github.com>
2019-05-06 02:54:52 +0200

    add print statements for logging

--- src/main.py
+++ src/main.py
@@ -25,10 +25,11 @@ def main(max_tries: int, use_grid: bool):
         overview_url = (
             f'{BASE_URL}/{current_date.year}/{current_date.month}/{current_date.day}/'
         )
-        r = requests.get(overview_url)
+        print('Getting overview from URL: ', overview_url)
         overview_response = requests.get(overview_url)
         has_successful_request = overview_response.ok
         if not overview_response.ok:
+            print('Bad response: ', overview_response)
             current_try += 1
             current_date -= timedelta(days=1)
             continue
@@ -47,7 +48,7 @@ def main(max_tries: int, use_grid: bool):
         # no slash between overview_url and last_upload_hour because it's already in overview_url
         image_url = f'{overview_url}{last_upload_hour}/{file_name}'
 
-    # http://www.sat.dundee.ac.uk/xrit/000.0E/MSG/2019/5/5/2200/2019_5_5_2200_MSG4_16_S1_grid.jpeg
+        print('Getting image from URL: ', image_url)
         image_response = requests.get(image_url)
         has_successful_request = image_response.ok
         if not image_response.ok:
@@ -57,18 +58,20 @@ def main(max_tries: int, use_grid: bool):
             continue
 
     if not has_successful_request:
+        print('No image has been successfully downloaded.')
         exit(1)
 
-    r = requests.get(image_url)
-    r.raise_for_status()
+    current_image_path = str(BASE_DIR / 'current.jpeg')
+    print('Saving image to: ', current_image_path)
+    with open(current_image_path, 'wb') as f:
+        f.write(image_response.content)
 
-    current_image_path = BASE_DIR / 'current.jpeg'
-    with open(str(current_image_path), 'wb') as f:
-        f.write(r.content)
-    with open(str(BASE_DIR / 'current.txt'), 'w') as f:
+    current_text_path = str(BASE_DIR / 'current.txt')
+    print(f'Writing newest filename {file_name} into {current_text_path }')
+    with open(current_text_path , 'w') as f:
         f.write(file_name)
 
-    subprocess.run(['feh', '--bg-max', str(current_image_path)])
+    subprocess.run(['feh', '--bg-max', current_image_path])
 
 
 if __name__ == '__main__':
