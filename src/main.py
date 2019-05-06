354b9549e90e0bd4885f4eacbb3f204cbe37ee1b
klausondrag <18405064+klausondrag@users.noreply.github.com>
2019-05-06 02:54:23 +0200

    rewrite loop to only quit if image download was successful, not just overview

--- src/main.py
+++ src/main.py
@@ -26,27 +26,38 @@ def main(max_tries: int, use_grid: bool):
             f'{BASE_URL}/{current_date.year}/{current_date.month}/{current_date.day}/'
         )
         r = requests.get(overview_url)
-        current_try += 1
-        has_successful_request = r.ok
-        if not r.ok:
+        overview_response = requests.get(overview_url)
+        has_successful_request = overview_response.ok
+        if not overview_response.ok:
+            current_try += 1
             current_date -= timedelta(days=1)
+            continue
+
+        soup = BeautifulSoup(overview_response.text, 'html.parser')
+        last_upload_hour = soup('tr')[-2]
+        last_upload_hour = last_upload_hour('a')[0].text
+        # remove trailing slash
+        last_upload_hour = last_upload_hour[:-1]
+
+        # http://www.sat.dundee.ac.uk/xrit/000.0E/MSG/2019/5/5/2200/2019_5_5_2200_MSG4_16_S1_grid.jpeg
+        file_name = (
+            f'{current_date.year}_{current_date.month}_{current_date.day}_{last_upload_hour}'
+            + f'_MSG4_16_S1{grid_text}.jpeg'
+        )
+        # no slash between overview_url and last_upload_hour because it's already in overview_url
+        image_url = f'{overview_url}{last_upload_hour}/{file_name}'
+
+    # http://www.sat.dundee.ac.uk/xrit/000.0E/MSG/2019/5/5/2200/2019_5_5_2200_MSG4_16_S1_grid.jpeg
+        image_response = requests.get(image_url)
+        has_successful_request = image_response.ok
+        if not image_response.ok:
+            print('Bad response: ', image_response)
+            current_try += 1
+            current_date -= timedelta(days=1)
+            continue
 
     if not has_successful_request:
         exit(1)
-
-    soup = BeautifulSoup(r.text, 'html.parser')
-    last_upload_hour = soup('tr')[-2]
-    last_upload_hour = last_upload_hour('a')[0].text
-    # remove trailing slash
-    last_upload_hour = last_upload_hour[:-1]
-
-    # http://www.sat.dundee.ac.uk/xrit/000.0E/MSG/2019/5/5/2200/2019_5_5_2200_MSG4_16_S1_grid.jpeg
-    file_name = (
-        f'{current_date.year}_{current_date.month}_{current_date.day}_{last_upload_hour}'
-        + f'_MSG4_16_S1{grid_text}.jpeg'
-    )
-    # no slash between overview_url and last_upload_hour because it's already in overview_url
-    image_url = f'{overview_url}{last_upload_hour}/{file_name}'
 
     r = requests.get(image_url)
     r.raise_for_status()
